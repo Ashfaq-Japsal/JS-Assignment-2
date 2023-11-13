3fdd60f690dc298eb45498191fe5cea755b1b0bd
Ashfaq <mybusinessisduty482@gmail.com>
2023-11-13 12:32:57 +0500

    2nd assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+// 1
+// var userName = "Ashfaq"
+// 2
+var myName = "Muhammad Ashfaq"
+//3
+var message = "Hello World"
+alert (message)
+//4
+var userName = prompt ("Enter your full name.")
+var userAge = prompt ("Enter your Age.")
+var userCourse = prompt ("Enter your course name.")
+alert ("Your full name: " + userName + "\nYour age: " + userAge + "\nYour course: " + userCourse)
+//5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert (pizza)
+//6
+var email = "muhammadashfaqqadri482@gmail.com"
+alert ("My email address is " + email)
+//7
+var book = "A smarter way to learn JavaScript "
+alert ("I am trying to learn from the Book " + book)
+//8
+document.write('Yah! I can write HTML content through JavaScript')
+//9
+var string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert (string)
